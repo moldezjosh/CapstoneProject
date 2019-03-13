8ce9bb51520efb5fd1b93d7a7458d7846babe7b3
Joshua Mark Moldez <j.markmoldez@gmail.com>
2019-03-14 01:44:15 +0800

    added camera

--- app/src/main/java/com/capstoneproject/capstone/MainActivity.java
+++ app/src/main/java/com/capstoneproject/capstone/MainActivity.java
@@ -1,6 +1,8 @@
 package com.capstoneproject.capstone;
 
+import android.content.Intent;
 import android.graphics.Bitmap;
+import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -11,9 +13,19 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-
+        dispatchTakePictureIntent();
 
     }
+
+    static final int REQUEST_IMAGE_CAPTURE = 1;
+
+    private void dispatchTakePictureIntent() {
+        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
+            startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
+        }
+    }
+
 
 
 }
