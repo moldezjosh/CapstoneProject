6d969e879ed131776e5113d800b88231a541610b
Joshua Mark Moldez <j.markmoldez@gmail.com>
2019-03-25 22:55:33 +0800

    added splash screen

--- app/src/main/java/com/capstoneproject/capstone/MainActivity.java
+++ app/src/main/java/com/capstoneproject/capstone/MainActivity.java
@@ -4,34 +4,26 @@ package com.capstoneproject.capstone;
  */
 
 import android.content.Intent;
-import android.graphics.Bitmap;
-import android.provider.MediaStore;
-import android.support.v7.app.AppCompatActivity;
+import android.os.Handler;
 import android.os.Bundle;
-
-import com.google.firebase.auth.FirebaseAuth;
+import android.support.v7.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
+    private static int SPLASH_TIME_OUT=2000;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-
-
-       // dispatchTakePictureIntent();
+        getSupportActionBar().hide();
+        setContentView(R.layout.activity_splash);
+        new Handler().postDelayed(new Runnable(){
+            @Override
+            public void run(){
+                Intent i = new Intent(MainActivity.this,LoginActivity.class);
+                startActivity(i);
+                finish();
+            }
+        }, SPLASH_TIME_OUT);
 
     }
-
-    static final int REQUEST_IMAGE_CAPTURE = 1;
-
-    private void dispatchTakePictureIntent() {
-        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-        if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
-            startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
-        }
-    }
-
-
-
 }
